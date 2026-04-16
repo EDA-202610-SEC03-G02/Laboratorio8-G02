91204e5a69ba64f70a9dadc3664da96af5e08ce7
Jucam1861 <j.castrom23@uniandes.edu.co>
2026-04-16 10:48:09 -0500

    primera version rotate right
    Implement right rotation for red-black tree.

---
+++ DataStructures/Tree/red_black_tree.py
@@ -0,0 +1,13 @@
+def rotate_right(bst):
+  root=bst["root"]
+  izq= root["left"]
+  if izq["color"]=="red" and izq["left"]["color"]=="red":
+    if izq["right"] is not None: 
+      temp= izq["right"]
+      root["left"]=None
+      izq["right"]=root
+      root=izq
+      root["right"]["left"]=temp #Primera version de rotate right
+      
+    
+    
